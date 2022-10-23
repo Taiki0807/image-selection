4a623d11e6b7a6515938a782e794f89bb1ae5368
Taiki0807 <hosokawataiki265@gmail.com>
2022-10-23 18:16:49 +0900

    [fix]Fixed line spacing

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,17 +2,11 @@ import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDfFfrYUz6i-Xfy3imO2CTWp5SFeA-yP_k",
-
     authDomain: "image-707ea.firebaseapp.com",
-  
     projectId: "image-707ea",
-  
     storageBucket: "image-707ea.appspot.com",
-  
     messagingSenderId: "377889197876",
-  
-    appId: "1:377889197876:web:ad60470c58c98aafee5f61"
-  
-}
+    appId: "1:377889197876:web:ad60470c58c98aafee5f61",
+};
 
 export const app = firebase.initializeApp(firebaseConfig);
