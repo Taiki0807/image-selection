7c98ad4ae7b526ecd81f966f5f93984589ae3226
Taiki0807 <hosokawataiki265@gmail.com>
2022-11-16 02:15:41 +0900

    [add]Add  setting Proxy

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,16 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware("/fastapi", {
+            target: "http://127.0.0.1:8080",
+            changeOrigin: true,
+        })
+    );
+    app.use(
+        createProxyMiddleware("/api", {
+            target: "https://my-go-api-tccvhcsvvq-an.a.run.app",
+            changeOrigin: true,
+        })
+    );
+};
